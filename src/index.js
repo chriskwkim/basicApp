ed20ad44c3903541d685fbe8b47597ef5c8e17c9
Chris Kim <chris.kim@kareo.com>
2019-11-16 22:32:33 -0800

    Refactored css to scss

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
-import './App.css';
+import './styles/style.scss';
 
 ReactDOM.render(
   <BrowserRouter>
